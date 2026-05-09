f51ab587793c06ffa04b30ee20e650e5d5a96c42
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-10 01:58:04 +0530

    Working with Multiple Users

--- spring-security-demo-2/src/main/java/com/itstech/dharm/springsecuritydemo/config/SecurityConfig.java
+++ spring-security-demo-2/src/main/java/com/itstech/dharm/springsecuritydemo/config/SecurityConfig.java
@@ -6,10 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,59 +19,37 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-/*
 
-        // THIS IMPERATIVE STYLE
-        */
-/*  FOR csrf *//*
-
-        Customizer<CsrfConfigurer<HttpSecurity>> csrfCustomizer =
-                new Customizer<CsrfConfigurer<HttpSecurity>>() {
-            @Override
-            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
-                httpSecurityCsrfConfigurer.disable();
-            }
-        };
-        http.csrf(csrfCustomizer);
-
-        */
-/*  FOR AuthorizeHttpRequests *//*
-
-        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> auth =
-                new Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
-                    @Override
-                    public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorization) {
-                        authorization.anyRequest().authenticated();
-                    }
-                };
-        http.authorizeHttpRequests(auth);
-
-        */
-/*  FOR AuthorizeHttpRequests *//*
-
-        Customizer<SessionManagementConfigurer<HttpSecurity>> session =
-                new Customizer<SessionManagementConfigurer<HttpSecurity>>() {
-            @Override
-            public void customize(SessionManagementConfigurer<HttpSecurity> sessionManagement) {
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-            }
-        };
-        http.sessionManagement(session);
-
-        return http.build();
-*/
-
-        // THIS DECLARATIVE STYLE
+       /*   fix: resolve CSRF token validation issue    */
        http
-                .csrf(
-                        AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        auth -> auth.anyRequest().authenticated())
-                //.formLogin(Customizer.withDefaults()) // NO NEED TO USED FOR STATELESS SESSION
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(
-                        session ->
-                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .csrf(
+                    AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                    auth -> auth.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(
+                    session ->
+                            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    /*  Working with Multiple Users */
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("dharm")
+                .password("dharm@123")
+                .roles("USER")
+                .build();
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
