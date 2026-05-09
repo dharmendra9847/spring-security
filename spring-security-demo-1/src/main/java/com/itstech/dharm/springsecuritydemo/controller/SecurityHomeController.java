a2382066f660cbc3a2961d973a261147d71c5c6d
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-10 01:16:50 +0530

    Initial spring-security setup

---
+++ spring-security-demo-1/src/main/java/com/itstech/dharm/springsecuritydemo/controller/SecurityHomeController.java
@@ -0,0 +1,19 @@
+package com.itstech.dharm.springsecuritydemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityHomeController {
+
+    @GetMapping("home")
+    public String home(HttpServletRequest request) {
+        return "Hello World! + \nYour sessionID is: " + request.getSession().getId();
+    }
+
+    @GetMapping("about")
+    public String about(HttpServletRequest request){
+        return "Hello, this about for me!, What do you want to do? \nYour sessionID is: " + request.getSession().getId();
+    }
+}
