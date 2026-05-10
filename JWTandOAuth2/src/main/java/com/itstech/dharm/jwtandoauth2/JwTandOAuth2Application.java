aabdf2d010c3ab3dc17b7f5768cb3cd47e140a7a
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-10 20:17:42 +0530

    Initial Jwt Project Setup

---
+++ JWTandOAuth2/src/main/java/com/itstech/dharm/jwtandoauth2/JwTandOAuth2Application.java
@@ -0,0 +1,13 @@
+package com.itstech.dharm.jwtandoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTandOAuth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwTandOAuth2Application.class, args);
+    }
+
+}
---
+++ JWTandOAuth2/src/test/java/com/itstech/dharm/jwtandoauth2/JwTandOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstech.dharm.jwtandoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwTandOAuth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
