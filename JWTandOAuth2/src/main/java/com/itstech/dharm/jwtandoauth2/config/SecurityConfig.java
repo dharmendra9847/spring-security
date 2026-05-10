8479eafea58528c6a4c8147d9c827b95e95d99fd
Dharmendra Kumar <dharmendra9847@gmail.com>
2026-05-11 00:49:28 +0530

    Custom Login

--- JWTandOAuth2/src/main/java/com/itstech/dharm/jwtandoauth2/config/SecurityConfig.java
+++ JWTandOAuth2/src/main/java/com/itstech/dharm/jwtandoauth2/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.itstech.dharm.jwtandoauth2.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -36,9 +38,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register")
+                        .requestMatchers("/register", "/login")
                         .permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest()
+                        .authenticated())
                 .sessionManagement(session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
@@ -46,6 +49,12 @@ public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- JWTandOAuth2/src/main/java/com/itstech/dharm/jwtandoauth2/controller/UserController.java
+++ JWTandOAuth2/src/main/java/com/itstech/dharm/jwtandoauth2/controller/UserController.java
@@ -3,6 +3,9 @@ package com.itstech.dharm.jwtandoauth2.controller;
 import com.itstech.dharm.jwtandoauth2.model.User;
 import com.itstech.dharm.jwtandoauth2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping("register")
+    @PostMapping("/register")
     public User register(@RequestBody User user) {
         userService.saveUser(user);
         return user;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        Authentication authentication;
+        authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return "success";
+        }
+
+        return "login failed";
     }
 }
